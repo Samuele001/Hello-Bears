ea0b85d43fffd4afb62e1f2bc7132cf3332f9a16
Samuele Bagli <38889654+Samuele001@users.noreply.github.com>
2020-09-07 22:22:25 +0200

    Update Hello Bears.cpp

--- Hello Bears.cpp
+++ Hello Bears.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	cout << "Hello Bears"<<endl;
 	cout << "Bears are so beautiful, right?" << endl;
-	cout << "If you want to learn more, browse on this site: https://www.youtube.com/watch?v=Zq9gy6MFrl4" << endl;
+	cout << "If you want to learn more, browse on this site: https://www.youtube.com/watch?v=5rAOFqF8L30" << endl;
 	system("pause");
 	return 0;
 }
